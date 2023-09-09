70be5329e61cc2c21e383d8b13e19095a08e0963
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-09 19:07:36 +0530

    login image

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,39 +1,39 @@
 import React from "react";
+import loginimg from "../../images/login-pic.jpg";
+import "./Login.css"
 
 export default function Login() {
   return (
     <div className="loginContainer">
       <div className="loginWrapper">
-        <div class="col">
-          <img src="./images/login-pic.jpg" alt="login img" class="login-img" />
+        <div class="imgcol">
+          <img src={loginimg} alt="login img" class="login-img" />
         </div>
-        <div class="col col2">
-          <div class="container form-container">
-            <h2>WELCOME BACK !!</h2>
-            <form action="">
-              <div class="mb-3 row">
-                <div class="col-sm-10">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="inputemail"
-                    placeholder="Email"
-                  />
-                </div>
+        <div class="formContainer">
+          <h2>WELCOME BACK !!</h2>
+          <form action="">
+            <div class="mb-3 row">
+              <div class="col-sm-10">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="inputemail"
+                  placeholder="Email"
+                />
               </div>
-              <div class="mb-3 row">
-                <div class="col-sm-10">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="inputPassword"
-                    placeholder="Password"
-                  />
-                </div>
+            </div>
+            <div class="mb-3 row">
+              <div class="col-sm-10">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="inputPassword"
+                  placeholder="Password"
+                />
               </div>
-              <button class="btn-primary">Submit</button>
-            </form>
-          </div>
+            </div>
+            <button class="btn-primary">Submit</button>
+          </form>
         </div>
       </div>
     </div>
