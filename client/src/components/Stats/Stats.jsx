9232af60922e55ce4ebb04f636a17e59137e9dee
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-10 13:28:47 +0530

    changes in stats page flex

--- client/src/components/Stats/Stats.jsx
+++ client/src/components/Stats/Stats.jsx
@@ -7,23 +7,45 @@ export default function Stats() {
   return (
     <div className="stats">
       <div className="statsContainer">
-        <div className="statsCard">
-          <h1>02</h1>
-          <p>Courses Completed</p>
-        </div>
-        <div className="statsCard">
-          <h1>02</h1>
-          <p>Courses Completed</p>
-        </div>
-        <div className="statsCard" id="stcd3">
-          <div className="statInfo">
-            <h1>8.5</h1>
-            <p>Hours Spent This Week</p>
+        <div className="Cards">
+          <div className="statsCard">
+            <h1>02</h1>
+            <p>Courses Completed</p>
           </div>
-          <StatsChart />
+          <div className="statsCard">
+            <h1>02</h1>
+            <p>Courses Completed</p>
+          </div>
+          <div className="statsCard" id="stcd3">
+            <div className="statInfo">
+              <h1>8.5</h1>
+              <p>Hours Spent This Week</p>
+            </div>
+            <StatsChart />
+          </div>
         </div>
+        <h2>My Assignments</h2>
+        <ul className="courseList">
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+        </ul>
       </div>
-      <Calendar />
+      <div className="col2">
+        <Calendar id="calender" />
+        <h2>Upcoming</h2>
+        <ul className="courseList">
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+          <li className="listItem">Course 1</li>
+        </ul>
+      </div>
     </div>
   );
 }
