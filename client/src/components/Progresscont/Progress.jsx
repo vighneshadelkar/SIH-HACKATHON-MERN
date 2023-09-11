48f408cafcfe295ada8692e21dcc5556f90e5680
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-11 23:26:59 +0530

    changed header

--- client/src/components/Progresscont/Progress.jsx
+++ client/src/components/Progresscont/Progress.jsx
@@ -7,30 +7,39 @@ export default function Progress() {
       <div className="progressWrapper">
         <div className="progressText">
           <small>Hi, Vighnesh!</small>
-          <h1>
-            You have completed 6 lessons this week
-          </h1>
+          <h1>You have completed 6 lessons this week</h1>
           <button className="pg-btn">SEE ALL</button>
         </div>
-        <div className="cards" >
-          <div className="cardContainer" data-aos="zoom-in-left" data-aos-duration="750">
+        <div className="cards">
+          <div
+            className="cardContainer"
+            data-aos="zoom-in-left"
+            data-aos-duration="750"
+          >
             <div className="card" id="cd1">
               <h2 className="title">Title</h2>
               <h3>Design Composition</h3>
               <span>100% COMPLETED</span>
-    
             </div>
           </div>
-          <div className="cardContainer" data-aos="zoom-in-left" data-aos-duration="1000">
+          <div
+            className="cardContainer"
+            data-aos="zoom-in-left"
+            data-aos-duration="1000"
+          >
             <div className="card" id="cd2">
               <div className="cardHead">
-                <h5 className="title">Title</h5>
+                <h2 className="title">Title</h2>
               </div>
               <h3>Design Composition</h3>
               <span>100% COMPLETED</span>
             </div>
           </div>
-          <div className="cardContainer" data-aos="zoom-in-left" data-aos-duration="1500">
+          <div
+            className="cardContainer"
+            data-aos="zoom-in-left"
+            data-aos-duration="1500"
+          >
             <div className="card" id="cd3">
               <h2 className="title">Title</h2>
               <h3>Design Composition</h3>
