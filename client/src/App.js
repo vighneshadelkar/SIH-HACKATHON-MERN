972c9f15bb74d90064a84df13556312bf117d30b
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-21 22:05:01 +0530

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import React, { useEffect} from 'react';
-import Profile from './pages/Profile/Profile';
-import Coursepg from './pages/Coursespg/Coursepg';
-import Aos from "aos"
-import Login from './pages/Login/Login';
-import { AuthUserProvider } from '../src/context/AuthContext';
-import Blogpg from './pages/Blogpage/Blogpg';
-import Home from './pages/Home/Home';
-import Event from './pages/Eventpg/Event';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import Profile from "./pages/Profile/Profile";
+import Coursepg from "./pages/Coursespg/Coursepg";
+import Aos from "aos";
+import Login from "./pages/Login/Login";
+import { AuthUserProvider } from "../src/context/AuthContext";
+import Blogpg from "./pages/Blogpage/Blogpg";
+import Home from "./pages/Home/Home";
+import Event from "./pages/Eventpg/Event";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   useEffect(() => {
     Aos.init();
   }, []);
 
   return (
-    <AuthUserProvider >
+    <AuthUserProvider>
       <div className="App">
         <BrowserRouter>
           <Routes>
@@ -30,9 +29,7 @@ function App() {
             <Route exact path="/event" element={<Event />}></Route>
           </Routes>
         </BrowserRouter>
-        <footer>
-          <Footer/>
-        </footer>
+        <Footer />
       </div>
     </AuthUserProvider>
   );
