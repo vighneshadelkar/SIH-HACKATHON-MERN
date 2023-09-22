cb5031d61caef18bf9a6daa2f0fbb66389cb42c0
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-22 12:22:51 +0530

    added isadmin in user model

--- api/models/users.js
+++ api/models/users.js
@@ -1,27 +1,31 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const userSchema=new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
     },
-    lastname:{
-        type:String,
-        required:true
+    lastname: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    token:{
-        type:String,
-        default:null
+    token: {
+        type: String,
+        default: null
     },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
 });
 
-module.exports=mongoose.model('Users',userSchema);
+module.exports = mongoose.model('Users', userSchema);
