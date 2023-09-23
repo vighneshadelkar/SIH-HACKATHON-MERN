0e76b78ab0ce457af92cf0f5bc036a33cd072296
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2023-09-23 16:18:34 +0530

    changed login heading

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
         </div>
         <div className="formContainer">
           <div>
-            <h2>SIGN UP</h2>
+            <h2>Welcome Back!! SIGN UP</h2>
           </div>
           <hr></hr>
           <form onSubmit={handleSubmit} className="loginForm">
